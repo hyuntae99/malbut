ef37cc4817c73b45a85b7179d818bf8a4622adda
hyuntae99 <hyuntae9912@naver.com>
2024-09-27 16:10:25 +0900

    리팩토링

--- src/main/java/com/hyunn/malBut/controller/QuizController.java
+++ src/main/java/com/hyunn/malBut/controller/QuizController.java
@@ -5,7 +5,6 @@ import com.hyunn.malBut.dto.response.GradeResponse;
 import com.hyunn.malBut.dto.response.QuizResponse;
 import com.hyunn.malBut.service.QuizService;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
