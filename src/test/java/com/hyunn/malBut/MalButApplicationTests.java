d29cb131d89c4b5c080a1c6569b2f5e37c968170
hyuntae99 <hyuntae9912@naver.com>
2024-09-28 22:45:25 +0900

    배포 오류 방지를 위한 Test Controller 처리

--- src/test/java/com/hyunn/malBut/MalButApplicationTests.java
+++ src/test/java/com/hyunn/malBut/MalButApplicationTests.java
@@ -1,13 +1,11 @@
 package com.hyunn.malBut;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MalButApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
